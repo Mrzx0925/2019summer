765556c637cfe2aef32f5e3d95ef0d2d55f0234e
Mrzx0925 <a1035395948@163.com>
2019-07-30 14:44:15 +0800

    ajax的demo

---
+++ AjaxDemo/src/com/zx/demo/JsonServlet.java
@@ -0,0 +1,47 @@
+package com.zx.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.zx.entity.Student;
+
+import net.sf.json.JSONObject;
+
+/**
+ * Servlet implementation class JsonServlet
+ */
+@WebServlet("/JsonServlet")
+public class JsonServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("utf-8");
+		PrintWriter	out = response.getWriter();
+		
+		Student stu1 = new Student();
+		stu1.setName("zx");
+		stu1.setAge(21);
+		JSONObject json = new JSONObject();
+		json.put("stu1", stu1);
+		out.print(json);
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
